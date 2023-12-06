f3f2a493452becec1e8f049c4a69348b9b6da158
Silvan Paul <silvan.paul123@gmail.com>
2023-12-07 00:00:55 +0100

    feat: finished day 6 and added zip method

--- lib/aoc2023.dart
+++ lib/aoc2023.dart
@@ -1,1 +1,7 @@
+Iterable<List<T>> zip<T>(Iterable<Iterable<T>> iterables) sync* {
+  final iterators = iterables.map((iterable) => iterable.iterator).toList();
 
+  while (iterators.every((iterator) => iterator.moveNext())) {
+    yield iterators.map((iterator) => iterator.current).toList();
+  }
+}
--- solutions/day06.dart
+++ solutions/day06.dart
@@ -1,3 +1,4 @@
+import 'package:aoc2023/aoc2023.dart';
 import '../utils/day_interface.dart';
 
 class Day06 extends AbstractDay {
@@ -6,8 +7,6 @@ class Day06 extends AbstractDay {
   @override
   parseInput() {
     // TODO: implement parseInput
-
-    // throw UnimplementedError();
   }
 
   @override
@@ -24,13 +23,51 @@ class Day06 extends AbstractDay {
 
     times = [numbers[0], numbers[1], numbers[2], numbers[3]];
     distances = [numbers[4], numbers[5], numbers[6], numbers[7]];
-    print(times);
+    int answer = 1;
 
-    return '';
+    for (var game = 0; game < distances.length; game++) {
+      int margin = 0;
+
+      for (int hold = 0; hold <= times[game]; hold++) {
+        if (hold * (times[game] - hold) > distances[game]) {
+          margin += 1;
+        }
+      }
+
+      answer *= margin;
+    }
+    return answer;
   }
 
   @override
   solvePart2() {
-    return 'uw vader';
+    List<int> time;
+    List<int> distance;
+    RegExp regExp = RegExp(r'-?\d+\.?\d*');
+    var localInput = regExp.allMatches(input.asString);
+
+    List<int> numbers = [];
+    for (Match match in localInput) {
+      numbers.add(int.parse(match.group(0)!));
+    }
+
+    time = [int.parse('${numbers[0]}${numbers[1]}${numbers[2]}${numbers[3]}')];
+    distance = [
+      int.parse('${numbers[4]}${numbers[5]}${numbers[6]}${numbers[7]}')
+    ];
+    int answer = 1;
+
+    for (var game = 0; game < distance.length; game++) {
+      int margin = 0;
+
+      for (int hold = 0; hold <= time[game]; hold++) {
+        if (hold * (time[game] - hold) > distance[game]) {
+          margin += 1;
+        }
+      }
+
+      answer *= margin;
+    }
+    return answer;
   }
 }
